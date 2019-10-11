71e50f56e2fba774060c58cc29e8109cb59e2ab6
Mitchell Nguyen <mmmitchellnguyen@gmail.com>
2019-10-11 12:40:30 -0700

    Finished basic UI for item-display screen (WIP want to add more organization to UI of search and item-display routes)

--- ebay_browse_app/lib/another_attempt.dart
+++ ebay_browse_app/lib/another_attempt.dart
@@ -18,6 +18,14 @@ class MyAppAttempt extends StatelessWidget {
   }
 }
 
+// 1. picture
+// 2. title
+// 3. price
+// 4. username of person who posted item
+// 5. location
+// .....
+// 5. further (extra) description about item
+
 class SecondScreen extends StatelessWidget {
   SecondScreen(this.item);
 
@@ -25,12 +33,154 @@ class SecondScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double textWidth = MediaQuery.of(context).size.width*0.8;
+
+//    print('price ' + item.price);
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Second Screen"),
       ),
-      body: Center(
-        child: new Text(item.title),
+      body: ListView(
+        padding: const EdgeInsets.all(8.0),
+        children: <Widget>[
+          Container(
+            height: 250,
+            child: FittedBox(
+              child: Center(child: Image.network(item.imageUrl)),
+            ),
+          ),
+          Container(
+            height: 20, //spacing
+          ),
+          Container(  // ITEM 2: title
+//            height: 50,
+            width: textWidth,
+            child: Center(child: Text(
+                item.title,
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                ),
+            )),
+          ),
+          Container(
+            height: item.subtitle != '' ? 20 : 0, //spacing
+          ),
+          Container(  // ITEM 3: SUBTITLE
+            width: textWidth,
+            child: Text(
+              item.subtitle,
+              style: TextStyle(fontSize: item.subtitle != '' ? 20 : 0),
+            ),
+          ),
+          Container(
+            height: 20, //spacing
+          ),
+          Container(  // ITEM 4: CATEGORY-NAME
+            width: textWidth,
+            child: Text(
+              'Category: ' + item.categoryName,
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+          Container(
+            height: 20, //spacing
+          ),
+          Container(  // ITEM 5: price
+            width: textWidth,
+            child: Text(
+              item.price != '' ? ('Price:  \$' + item.price): ('Price:  (Price not listed)'),
+              style: TextStyle(
+                fontSize: 15,
+              ),
+            ),
+          ),
+          Container(
+            height: 20, //spacing
+          ),
+          Container(  // ITEM 5: Payment methods
+            width: textWidth,
+            child: Text(
+              item.paymentMethods != '' ? ('Payment Methods:  ' + item.paymentMethods): ('Payment Methods:  (Payment Methods not listed)'),
+              style: TextStyle(
+                fontSize: 15,
+              ),
+            ),
+          ),
+          Container(
+            height: 20, //spacing
+          ),
+          Container(  // ITEM 4: itemId
+            width: textWidth,
+            child: Text(
+              item.itemId != '' ? ('Item ID: ' + item.itemId): ('Item ID:  (Item ID not listed)'),
+              style: TextStyle(
+                fontSize: 15,
+              ),
+            ),
+          ),
+          Container(
+            height: 20, //spacing
+          ),
+          Container(  // ITEM 5: productId
+            width: textWidth,
+            child: Text(
+              item.productId != '' ? ('Product ID: ' + item.productId): ('Product ID:  (Product ID not listed)'),
+              style: TextStyle(
+                fontSize: 15,
+              ),
+            ),
+          ),
+          Container(
+            height: 20, //spacing
+          ),
+          Container(  // ITEM 6: location
+            width: textWidth,
+            child: Text(
+              item.location != null ? ('Location:  ' + item.location): ('Location:  (Location not listed).'),
+              style: TextStyle(
+                fontSize: 15,
+              ),
+            ),
+          ),
+          Container(
+            height: 20, //spacing
+          ),
+          Container(  // ITEM 5: Shipping Service Cost
+            width: textWidth,
+            child: Text(
+              item.shippingServiceCost != '' ? ('Shipping Service Cost: ' + item.shippingServiceCost): ('Shipping Service Cost:  (Shipping Service Cost not listed)'),
+              style: TextStyle(
+                fontSize: 15,
+              ),
+            ),
+          ),
+          Container(
+            height: 20, //spacing
+          ),
+          Container(  // ITEM 5: Shipping Type
+            width: textWidth,
+            child: Text(
+              item.shippingType != '' ? ('Shipping Type: ' + item.shippingType): ('Shipping Type:  (Shipping Type not listed)'),
+              style: TextStyle(
+                fontSize: 15,
+              ),
+            ),
+          ),
+          Container(
+            height: 20, //spacing
+          ),
+          Container(  // ITEM 5: shipping to locations
+            width: textWidth,
+            child: Text(
+              item.shippingToLocations != '' ? ('Available Shipping Locations: ' + item.shippingToLocations): ('Available Shipping Locations:  (Available Shipping Locations not listed)'),
+              style: TextStyle(
+                fontSize: 15,
+              ),
+            ),
+          ),
+        ]
       ),
     );
   }
@@ -85,13 +235,15 @@ class _MyHomePageState extends State<MyHomePage> {
       var results = document.findAllElements('item');
       var data = results.toList(); // transform Iterable to a List
 
-      print(data[0].runtimeType);
+//      print(data[0].runtimeType);
+//      print(document.toXmlString(pretty: true, indent: '\t'));
 
       for (var i = 0; i < data.length; i++) {
-        print("hi");
+//        print("hi");
         _addSearchItem(data[i]);
       }
     } else {
+      print('error occurred');
       _onError();
     }
 
@@ -101,15 +253,15 @@ class _MyHomePageState extends State<MyHomePage> {
 
 
 
-    print(_items.length);
-    print(_items[0]);
+//    print(_items.length);
+//    print(_items[0]);
   }
 
   String reformatText(String text) {
     if (text.contains(" ")) {
       text = text.split(" ").join("%20");
     }
-
+//    print('textttt ' + text);
     return text;
   }
 
@@ -125,17 +277,83 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  // REFERENCE:
+  // <https://stackoverflow.com/questions/57186404/how-to-get-substring-between-two-strings-in-dart>
+  String _getNestedTagContent(String text, String startTag, String endTag) {
+    final startIndex = text.indexOf(startTag);
+    final endIndex = text.indexOf(endTag, startIndex + startTag.length);
+
+//    print('start idx: ' + startIndex.toString());
+//    print('end idx: ' + endIndex.toString());
+
+    // in the case that we don't find the particular tag in the item's XML description
+    if (startIndex == -1 || endIndex == -1) {
+      return '';
+    }
+
+    return text.substring(startIndex + startTag.length, endIndex);
+  }
+
   void _addSearchItem(xml.XmlElement item) {
 
-    print("IMAGEEEE: " + removeAllXmlTags(item.findElements("galleryURL").toString()));
+    // extract nested-tag for "currentPrice"
+    var sellingStatusText = item.findElements("sellingStatus").toString();
+    var currentPrice = _getNestedTagContent(
+        sellingStatusText,
+        '\<currentPrice currencyId=\"USD\"\>',
+        '\<\/currentPrice\>'
+    );
+
+    // extract nested-tag for "categoryName"
+    var primaryCategoryText = item.findElements("primaryCategory").toString();
+    var categoryName = _getNestedTagContent(
+        primaryCategoryText,
+        '\<categoryName\>',
+        '\<\/categoryName\>'
+    );
+
+    // extract nested-tag for "shippingServiceCost"
+    var shippingInfoText = item.findElements("shippingInfo").toString();
+    var shippingServiceCost = _getNestedTagContent(
+        shippingInfoText,
+        '\<shippingServiceCost currencyId=\"USD\"\>',
+        '\<\/shippingServiceCost\>'
+    );
+
+    // extract nested-tag for "shippingType"
+    var shippingType = _getNestedTagContent(
+        shippingInfoText,
+        '\<shippingType\>',
+        '\<\/shippingType\>'
+    );
+
+    // extract nested-tag for "shippingToLocations"
+    var shippingToLocations = _getNestedTagContent(
+        shippingInfoText,
+        '\<shipToLocations\>',
+        '\<\/shipToLocations\>'
+    );
 
     setState(() {
       _items.add(new SearchItem(
           removeAllXmlTags(item.findElements("title").toString()),
+          removeAllXmlTags(item.findElements("subtitle").toString()),
           removeAllXmlTags(item.findElements("galleryURL").toString()),
           removeAllXmlTags(item.findElements("location").toString()),
-          removeAllXmlTags(item.findElements("currentPrice").toString()),
+          currentPrice + ' USD',
+          removeAllXmlTags(item.findElements("itemId").toString()),
+          removeAllXmlTags(item.findElements("productId").toString()),
+          categoryName,
+          shippingServiceCost,
+          shippingType,
+          shippingToLocations,
+          removeAllXmlTags(item.findElements("paymentMethod").toString()),
       ));
+
+//      print('payment methods: ' + removeAllXmlTags(item.findElements("paymentMethod").toString()));
+
+//      print(item.findElements("sellingStatus").toList());
+
     });
   }
 
@@ -223,11 +441,43 @@ class _MyHomePageState extends State<MyHomePage> {
 }
 
 class SearchItem {
-  String title, imageUrl, location, price;
-  SearchItem(String title, String imageUrl, String location, String price) {
+  String title,
+      subtitle,
+      imageUrl,
+      location,
+      price,
+      itemId,
+      productId,
+      categoryName,
+      shippingServiceCost,
+      shippingType,
+      shippingToLocations,
+      paymentMethods
+  ;
+
+  SearchItem(String title,
+      String subtitle,
+      String imageUrl,
+      String location,
+      String price,
+      String itemId,
+      String productId,
+      String categoryName,
+      String shippingServiceCost,
+      String shippingType,
+      String shippingToLocations,
+      String paymentMethods) {
     this.title = title;
+    this.subtitle = subtitle;
     this.imageUrl = imageUrl;
     this.location = location;
     this.price = price;
+    this.itemId = itemId;
+    this.productId = productId;
+    this.categoryName = categoryName;
+    this.shippingServiceCost = shippingServiceCost;
+    this.shippingType = shippingType;
+    this.shippingToLocations = shippingToLocations;
+    this.paymentMethods = paymentMethods;
   }
 }
